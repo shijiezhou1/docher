acc703a9cc99f48d69cb5866d090acdd344caa32
Shijie Zhou <cs5129606@gmail.com>
2020-06-03 21:41:08 +0800

    Dev version

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -1,9 +1,10 @@
-var http = require('http');
+let http = require('http');
 
-var server = http.createServer((req, res)=>{
-    res.writeHead(200, {"Content-Type":"text/plain"})
-    res.end("Welcome to Nodejs - " + process.env.APP_NAME)
+let port = process.env.PORT ? process.env.PORT : 4000
+let server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" })
+    res.end(`Welcome to Nodejs : ${process.env.APP_NAME} : ${port}`)
 })
 
-server.listen(3000);
-console.log('Running in ports : 3000');
+server.listen(port);
+console.log('Running in ports : ' + port);
